47e1ecc3d4066e4ee3df22b0b659d730b6bb5346
Simon Königsreiter <simon.koenigsreiter@gmail.com>
2019-02-21 09:44:11 +0100

    Adds Headers

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,3 +1,9 @@
+/*
+ * Author: Königsreiter Simon, Klimont Joel, Lampalzer Alexander, Glavanits Marcel
+ * Class: 5BHIF
+ * Date: 14.02.2019
+ */
+
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
